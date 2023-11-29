70d392dc41e06cfa8e18d69531dc1c332eff7ea9
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2023-11-30 00:16:43 +0100

    adds test data

---
+++ src/main/resources/db/migration/V2__data.sql
@@ -0,0 +1,4 @@
+INSERT INTO "user" (`first_name`, `last_name`, `email`)
+VALUES ('John', 'Smith', 'john@gmail.com'),
+       ('Jane', 'Austen', 'jane@gmail.com'),
+       ('Joe', 'Schmoe', 'joe@gmail.com');
