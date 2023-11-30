b743fe5343376c54a72efd74c47f16a0c6d58131
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2023-11-30 13:22:30 +0100

    create user mapping

--- src/main/java/com/example/graphqldemo/controllers/UserController.java
+++ src/main/java/com/example/graphqldemo/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.example.graphqldemo.controllers;
 import com.example.graphqldemo.entities.User;
 import com.example.graphqldemo.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -14,12 +16,17 @@ public class UserController {
     private final UserService userService;
 
     @QueryMapping(name = "user")
-    public User getUser(Long id) {
+    public User getUser(@Argument Long id) {
         return userService.getUser(id);
     }
 
     @QueryMapping(name = "users")
     public List<User> getUsers() {
         return userService.getUsers();
+    }
+
+    @MutationMapping
+    public User createUser(@Argument User user) {
+        return userService.createUser(user);
     }
 }
